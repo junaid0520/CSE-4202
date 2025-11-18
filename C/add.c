c4c1f9a7d2610030dd10a0488cf344f0a46d7ef0
junaid0520 <akanda-25024015@aaub.edu.bd>
2025-11-18 12:57:50 +0600

    Add files via upload

---
+++ C/add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,s;
+printf{"Enter first number : "};
+scanf{"%d",&a};
+
+  printf{"Enter second number : "};
+  scanf{"%d",&b};
+
+  s = a+b;
+
+  printf{"Sum is %d",s};
+
+  return 0;
+}
